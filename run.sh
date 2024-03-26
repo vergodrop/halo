d1a0d83226cdad45ff4e7664e937accb20d5a5bc
vergo <165070430+vergodrop@users.noreply.github.com>
2024-03-27 00:45:49 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://RPCETHLAVA/)
+curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-326bc254-5ddb-43b6-8d63-a75f7b136d53/)
 
 
 echo "$curl_output" >> Update.tsx
